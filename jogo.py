261efe45327c31c73dd6efff3140a50417467458
Matheus Mizrahi <matheusm3@al.insper.edu.br>
2024-05-06 17:02:16 -0300

    inicio pygame

--- jogo.py
+++ jogo.py
@@ -1,1 +1,13 @@
-print('A')
+#====== Inicialização=======
+
+#---------- Impoprta e inicia pacotes
+import pygame
+import random
+
+pygame.init()
+
+#---------- Gera tela principal
+WIDTH=400
+HEIGHT=500
+
+
