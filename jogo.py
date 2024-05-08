3d56d133405cbb652125c33e614ae7ebbe73dcff
Matheus Mizrahi <matheusm3@al.insper.edu.br>
2024-05-08 13:52:25 -0300

    ínicio jogo , começor a definir a classe Brick (tijolo)

--- jogo.py
+++ jogo.py
@@ -1,8 +1,7 @@
 #====== Inicialização=======
 
-#---------- Impoprta e inicia pacotes
+#---------- Importa e inicia pacotes
 import pygame
-
 import random
 
 pygame.init()
@@ -10,5 +9,38 @@ pygame.init()
 #---------- Gera tela principal
 WIDTH=400
 HEIGHT=500
+window = pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption('BREAKOUT INSPER')
+
+
+
+
+#---------- Inicia estrutura de dados
+#definindo os novos tipos
+
+class Brick(pygame.sprite.Sprite):
+    def __init__(self,img)
+
+game=True
+#========loop principal========
+while game:
+    # ----- Trata eventos
+    for event in pygame.event.get():
+        # ----- Verifica consequências
+        if event.type == pygame.QUIT:
+            game = False
+        
+    # ----- Gera saídas
+
+
+    # ----- Atualiza estado de jogo
+    pygame.display.update()
+
+#======Finalização=======
+pygame.quit()
+
+
+
+
 
 
